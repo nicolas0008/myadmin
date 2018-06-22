8497e6b7ca56712e134babea53b8f25d9d4b02e3
nitillet <nicolas.tillet@millenniumhealth.com>
2018-06-22 03:17:21 -0300

    First commit with store/effects installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,23 @@
 import { Component } from '@angular/core';
 import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  query,
+    trigger,
+    state,
+    style,
+    animate,
+    transition,
+    query,
 } from '@angular/animations'
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+
 
 })
 export class AppComponent {
-  title = 'app';
-  getRouteAnimation(outlet) {
-      
-      return outlet.activatedRouteData.animation
-  }
+    title = 'app';
+    getRouteAnimation(outlet) {
+
+        return outlet.activatedRouteData.animation
+    }
 }
